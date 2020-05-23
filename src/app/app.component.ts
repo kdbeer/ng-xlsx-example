0efae37da37fd550efc26128d87bd91f2207be56
kdbeer <kridsadacpe@gmail.com>
2020-05-23 23:34:36 +0700

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
 import { Component } from '@angular/core';
+import * as FileSaver from 'file-saver';
+import * as XLSX from 'xlsx';
+
+const EXCEL_TYPE =
+  'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'ng-xlsx-example';
+  employee = [
+    { name: 'Shyam', email: 'shyamjaiswal@gmail.com' },
+    { name: 'Bob', email: 'bob32@gmail.com' },
+    { name: 'Jai', email: 'jai87@gmail.com' },
+  ];
+
+  exportToExcel() {
+    const worksheet: XLSX.WorkSheet = XLSX.utils.json_to_sheet(this.employee);
+    const workbook: XLSX.WorkBook = {
+      Sheets: { Sheet1: worksheet },
+      SheetNames: ['Sheet1'],
+    };
+
+    const excelBuffer: any = XLSX.write(workbook, {
+      bookType: 'xlsx',
+      type: 'array',
+    });
+
+    const data: Blob = new Blob([excelBuffer], { type: EXCEL_TYPE });
+    const date = new Date();
+    const fileName = 'example.xlsx';
+
+    FileSaver.saveAs(data, fileName);
+  }
 }
